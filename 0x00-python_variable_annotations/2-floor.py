91c6324a4edab3576fcd8e50f75c352006c10555
riyo3350g <yechouben@gamail.com>
2024-01-11 13:42:27 +0100

    Add Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Float to floor module"""
+
+
+def floor(n: float) -> int:
+    """
+    Function that take a float n as argument and returns the floor of the float
+    """
+    return int(n)
