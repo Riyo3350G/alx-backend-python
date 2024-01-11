9ece880f4c60b8bedeada4c67a3b4685a529e501
riyo3350g <yechouben@gamail.com>
2024-01-11 13:36:10 +0100

    Add Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Concat module and annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that concatenates two strings and returns the concatenated string
+    """
+    return str1 + str2
