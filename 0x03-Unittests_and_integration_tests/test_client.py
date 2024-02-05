f92ae614fe7ac74e9a59da10fd9cf8f9d69d01ca
riyo3350g <yechouben@gamail.com>
2024-02-05 14:18:15 +0100

    Add Task 7 Req

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,3 +42,14 @@ class TestGithubOrgClient(unittest.TestCase):
             test_class.public_repos(),
             [repo["name"] for repo in mock_get.return_value]
         )
+
+    @parameterized.expand([
+       ({"license": {"key": "my_license"}}, "my_license", True),
+       ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test has_license"""
+        self.assertEqual(
+            GithubOrgClient.has_license(repo, license_key),
+            expected
+        )
